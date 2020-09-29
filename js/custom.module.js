70392308d1d2825eec292d3e8b37472cfcb7ff85
Bill Kelm <bkelm@willamette.edu>
2020-09-29 15:39:36 -0700

    EXLIBRIS-632

--- js/custom.module.js
+++ js/custom.module.js
@@ -2,10 +2,15 @@
 
 /* We use a CENTRAL_PACKAGE, so use the below line to bootstrap the module */
 
-var app = angular.module('viewCustom', ['angularLoad', 'reportProblem', 'libraryh3lpWidget', 'giftBooks', 'toggleInstitutions', 'myILL', 'oadoi', 'customActions', 'smsAction', 'hathiTrustAvailability']);
+var app = angular.module('viewCustom', ['angularLoad', 'reportProblem', 'libraryh3lpWidget', 'giftBooks', 'toggleInstitutions', 'myILL', 'oadoi', 'customActions', 'smsAction', 'hathiTrustAvailability', 'externalSearch']);
 
 /************************************* END Bootstrap Script ************************************/
 
 app.component('prmSearchResultAvailabilityLineAfter', {
   template: '<hathi-trust-availability hide-online="false" entity-id="https://idp.willamette.edu/idp"></hathi-trust-availability>'
 });
+
+app
+.component('prmFacetAfter', {template: '<external-search-facet />'})
+.component('prmPageNavMenuAfter', {template: '<external-search-pagenav />' })
+.component('prmFacetExactAfter', {template: '<external-search-contents />' });
